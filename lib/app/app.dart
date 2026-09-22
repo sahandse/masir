bc735c112cfc55598c21799edaba630932504d1a
Sahand Rezvan <sahand.rezvanazar@gmail.com>
2026-09-22 11:55:11 +0330

    feat: add lib/app/app.dart

---
+++ lib/app/app.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:masir/app/theme.dart';
+import 'package:masir/features/map/presentation/map_page.dart';
+
+class MasirApp extends StatelessWidget {
+  const MasirApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'مسیر',
+      locale: const Locale('fa', 'IR'),
+      supportedLocales: const [Locale('fa', 'IR'), Locale('en', 'US')],
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      theme: MasirTheme.light,
+      darkTheme: MasirTheme.dark,
+      themeMode: ThemeMode.system,
+      home: const Directionality(
+        textDirection: TextDirection.rtl,
+        child: MapPage(),
+      ),
+    );
+  }
+}
